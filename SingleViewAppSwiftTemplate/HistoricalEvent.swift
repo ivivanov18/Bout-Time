4701e192f4019b91aaf142678bf0cad12e7ba52b
ivivanov18 <ivanov.ivan.v@gmail.com>
2018-09-20 16:32:49 +0300

    add struct HistoricalEvent
    before rename

---
+++ SingleViewAppSwiftTemplate/HistoricalEvent.swift
@@ -0,0 +1,16 @@
+//
+//  HistoricalEvent.swift
+//  SingleViewAppSwiftTemplate
+//
+//  Created by ivan ivanov on 20/09/2018.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct HistoricalEvent{
+    
+    var description: String
+    var year: String
+    
+}
